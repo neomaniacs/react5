890481f01d5334f450617ad696e10c101dc01ab3
Laurent FIDAHOUSSEN <10000.maniacs@gmail.com>
2022-12-29 14:47:22 +0100

    last updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useContext } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./store/auth-context";
 
 function App() {
   const ctx = useContext(AuthContext) ;
